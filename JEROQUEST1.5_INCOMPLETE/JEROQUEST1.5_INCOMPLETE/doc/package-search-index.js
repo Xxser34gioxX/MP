75d2a603a280fad65f2ffe33dd062374de4f915a
sergio <sergiofontoria@gmail.com>
2025-03-22 16:00:44 +0100

    FCR

---
+++ JEROQUEST1.5_INCOMPLETE/JEROQUEST1.5_INCOMPLETE/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"jeroquest.boardgame"},{"l":"jeroquest.gui"},{"l":"jeroquest.logic"},{"l":"jeroquest.test"},{"l":"jeroquest.units"},{"l":"jeroquest.utils"}]
